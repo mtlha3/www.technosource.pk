62bd28883a43819eea2885328adc02ffba7d2e0e
mtlha3 <mohdtlha3@gmail.com>
2024-09-01 18:12:08 +0500

    Finalizing

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,14 +1,13 @@
-import {React, useState }from 'react';
+import { React, useState } from 'react';
 import Swal from 'sweetalert2';
 import emailjs from '@emailjs/browser';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from "../components/Nav";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import './Contact.css';
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 
 const ContactUs = () => {
-
 
   emailjs.init('t7dOPoeM5-GAvu6Um');
   
@@ -18,20 +17,16 @@ const ContactUs = () => {
       html: `
         <form id="contact-form">
           <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" class="form-control" required>
+            <input type="text" id="name" name="name" class="form-control" required placeholder="Name">
           </div>
           <div class="form-group">
-            <label for="no">No</label>
-            <input type="text" id="no" name="no" class="form-control" required>
+            <input type="text" id="no" name="no" class="form-control" required placeholder="Phone Number">
           </div>
           <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" class="form-control" required>
+            <input type="email" id="email" name="email" class="form-control" required placeholder="Email">
           </div>
           <div class="form-group">
-            <label for="comment">Comment</label>
-            <textarea id="comment" name="comment" class="form-control" rows="3" required></textarea>
+            <textarea id="comment" name="comment" class="form-control" rows="3" required placeholder="Your Query"></textarea>
           </div>
         </form>
       `,
@@ -42,6 +37,12 @@ const ContactUs = () => {
         const form = Swal.getPopup().querySelector('#contact-form');
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
+
+        // Check if any field is empty
+        if (!data.name || !data.no || !data.email || !data.comment) {
+          Swal.showValidationMessage('Please fill out all fields');
+          return false; // Prevent the form from being submitted
+        }
 
         return emailjs.send('service_zjhtl3u', 'template_lx3t7wr', data)
           .then(response => response)
@@ -66,59 +67,58 @@ const ContactUs = () => {
   });
 
   return (
-
     <>
-    <Nav/>
-    <div className="contact-page">
-    <div className="contact-container">
-        <h3>
-          <span className="welcome-text">
-            {contactText}
-            {!isTypingComplete && <Cursor cursorStyle="|" cursorColor="white" />}
-          </span>
-        </h3>
-      </div>
-      <div>
-        <h3 style={{ color: 'black', marginTop: '25px', marginLeft: '20px' }}>Techno Source</h3>
-      </div>
+      <Nav />
+      <div className="contact-page">
+        <div className="contact-container">
+          <h3>
+            <span className="welcome-text">
+              {contactText}
+              {!isTypingComplete && <Cursor cursorStyle="|" cursorColor="white" />}
+            </span>
+          </h3>
+        </div>
+        <div>
+          <h3 style={{ color: 'black', marginTop: '25px', marginLeft: '20px' }}>Techno Source</h3>
+        </div>
 
-      <div className="details">
-        <table className="contact-table">
-          <tbody>
-            <tr>
-              <td className="contact-label">Phone No <span><i className="fa-solid fa-phone"></i></span> :</td>
-              <td className="contact-info">+92-213-5387140</td>
-            </tr>
-            <tr>
-              <td className="contact-label">Cell <span><i className="fa-solid fa-phone-volume"></i></span> :</td>
-              <td className="contact-info">
-                +92-323-0000219 <br />
-                +92-334-2193000
-              </td>
-            </tr>
-            <tr>
-              <td className="contact-label">Whatsapp <span><i className="fa-brands fa-square-whatsapp"></i> :</span></td>
-              <td className="contact-info">+92-334-2193000</td>
-            </tr>
-            <tr>
-              <td className="contact-label">Email <span><i className="fa-solid fa-envelope"></i></span> :</td>
-              <td className="contact-info">
-                info@technosource.pk <br />
-                tecsource@yahoo.com
-              </td>
-            </tr>
-            <tr>
-              <td className="contact-label">Facebook <span><i className="fa-brands fa-facebook" style={{ color: '#000000' }}></i></span> :</td>
-              <td className="contact-info"><a href="" style={{textDecoration: "none", color:"black"}}><span><i className="fa-brands fa-square-facebook"></i></span> Facebook Page</a></td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="contact-form-container">
-          <button id="contact-form-button" className="btn btn-primary" onClick={handleContactClick}>Click to write your queries</button>
+        <div className="details">
+          <table className="contact-table">
+            <tbody>
+              <tr>
+                <td className="contact-label">Phone No <span><i className="fa-solid fa-phone"></i></span> :</td>
+                <td className="contact-info">+92-213-5387140</td>
+              </tr>
+              <tr>
+                <td className="contact-label">Cell <span><i className="fa-solid fa-phone-volume"></i></span> :</td>
+                <td className="contact-info">
+                  +92-323-0000219 <br />
+                  +92-334-2193000
+                </td>
+              </tr>
+              <tr>
+                <td className="contact-label">Whatsapp <span><i className="fa-brands fa-square-whatsapp"></i> :</span></td>
+                <td className="contact-info">+92-334-2193000</td>
+              </tr>
+              <tr>
+                <td className="contact-label">Email <span><i className="fa-solid fa-envelope"></i></span> :</td>
+                <td className="contact-info">
+                  info@technosource.pk <br />
+                  tecsource@yahoo.com
+                </td>
+              </tr>
+              <tr>
+                <td className="contact-label">Facebook <span><i className="fa-brands fa-facebook" style={{ color: '#000000' }}></i></span> :</td>
+                <td className="contact-info"><a href="" style={{textDecoration: "none", color:"black"}}><span><i className="fa-brands fa-square-facebook"></i></span> Facebook Page</a></td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="contact-form-container">
+            <button id="contact-form-button" className="btn btn-primary" onClick={handleContactClick}>Click to write your queries</button>
+          </div>
         </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
